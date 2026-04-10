a45538fff81d87cb5cdebb920b11deb16c8c05bc
Zayvora Engine <zayvora@viadecide.com>
2026-04-10 08:49:55 +0530

    fix: add catch-all route handler for /api/* sub-paths to fix Vercel routing

---
+++ api/[...].js
@@ -0,0 +1,6 @@
+// Catch-all route handler for /api/* paths
+// This file ensures all /api/* requests are routed to the main handler logic
+
+import handler from './index.js';
+
+export default handler;
